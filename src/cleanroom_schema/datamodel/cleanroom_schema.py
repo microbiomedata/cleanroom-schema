e63ccf335c50c8d4d08956c2f7954eb857c90853
Mark A. Miller <mamillerpa@gmail.com>
2022-12-07 11:50:38 -0500

    neutral modelling test and data

--- src/cleanroom_schema/datamodel/cleanroom_schema.py
+++ src/cleanroom_schema/datamodel/cleanroom_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from cleanroom_schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-12-07T11:14:41
+# Generation date: 2022-12-07T11:47:32
 # Schema: cleanroom-schema
 #
 # id: https://w3id.org/microbiomedata/cleanroom-schema
@@ -22,8 +22,8 @@ from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
 from linkml_runtime.utils.enumerations import EnumDefinitionImpl
 from rdflib import Namespace, URIRef
 from linkml_runtime.utils.curienamespace import CurieNamespace
-from linkml_runtime.linkml_model.types import Date, Integer, String, Uriorcurie
-from linkml_runtime.utils.metamodelcore import URIorCURIE, XSDDate
+from linkml_runtime.linkml_model.types import String, Uriorcurie
+from linkml_runtime.utils.metamodelcore import URIorCURIE
 
 metamodel_version = "1.7.0"
 version = None
@@ -32,12 +32,9 @@ version = None
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
-PATO = CurieNamespace('PATO', 'http://purl.obolibrary.org/obo/PATO_')
-BIOLINK = CurieNamespace('biolink', 'https://w3id.org/biolink/')
 CLEANROOM_SCHEMA = CurieNamespace('cleanroom_schema', 'https://w3id.org/microbiomedata/cleanroom-schema/')
-EXAMPLE = CurieNamespace('example', 'https://example.org/')
 LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
-SCHEMA = CurieNamespace('schema', 'http://schema.org/')
+SCHEMA = CurieNamespace('schema', 'http://example.org/UNKNOWN/schema/')
 DEFAULT_ = CLEANROOM_SCHEMA
 
 
@@ -45,10 +42,6 @@ DEFAULT_ = CLEANROOM_SCHEMA
 
 # Class references
 class NamedThingId(URIorCURIE):
-    pass
-
-
-class PersonId(NamedThingId):
     pass
 
 
@@ -84,79 +77,27 @@ class NamedThing(YAMLRoot):
 
 
 @dataclass
-class Person(NamedThing):
+class DataLists(YAMLRoot):
     """
-    Represents a Person
-    """
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = CLEANROOM_SCHEMA.Person
-    class_class_curie: ClassVar[str] = "cleanroom_schema:Person"
-    class_name: ClassVar[str] = "Person"
-    class_model_uri: ClassVar[URIRef] = CLEANROOM_SCHEMA.Person
-
-    id: Union[str, PersonId] = None
-    primary_email: Optional[str] = None
-    birth_date: Optional[Union[str, XSDDate]] = None
-    age_in_years: Optional[int] = None
-    vital_status: Optional[Union[str, "PersonStatus"]] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, PersonId):
-            self.id = PersonId(self.id)
-
-        if self.primary_email is not None and not isinstance(self.primary_email, str):
-            self.primary_email = str(self.primary_email)
-
-        if self.birth_date is not None and not isinstance(self.birth_date, XSDDate):
-            self.birth_date = XSDDate(self.birth_date)
-
-        if self.age_in_years is not None and not isinstance(self.age_in_years, int):
-            self.age_in_years = int(self.age_in_years)
-
-        if self.vital_status is not None and not isinstance(self.vital_status, PersonStatus):
-            self.vital_status = PersonStatus(self.vital_status)
-
-        super().__post_init__(**kwargs)
-
-
-@dataclass
-class PersonCollection(YAMLRoot):
-    """
-    A holder for Person objects
+    A datastructure that can contain lists of instances from any selected classes in the schema
     """
     _inherited_slots: ClassVar[List[str]] = []
 
-    class_class_uri: ClassVar[URIRef] = CLEANROOM_SCHEMA.PersonCollection
-    class_class_curie: ClassVar[str] = "cleanroom_schema:PersonCollection"
-    class_name: ClassVar[str] = "PersonCollection"
-    class_model_uri: ClassVar[URIRef] = CLEANROOM_SCHEMA.PersonCollection
+    class_class_uri: ClassVar[URIRef] = CLEANROOM_SCHEMA.DataLists
+    class_class_curie: ClassVar[str] = "cleanroom_schema:DataLists"
+    class_name: ClassVar[str] = "DataLists"
+    class_model_uri: ClassVar[URIRef] = CLEANROOM_SCHEMA.DataLists
 
-    entries: Optional[Union[Dict[Union[str, PersonId], Union[dict, Person]], List[Union[dict, Person]]]] = empty_dict()
+    nt_list: Optional[Union[Dict[Union[str, NamedThingId], Union[dict, NamedThing]], List[Union[dict, NamedThing]]]] = empty_dict()
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        self._normalize_inlined_as_dict(slot_name="entries", slot_type=Person, key_name="id", keyed=True)
+        self._normalize_inlined_as_dict(slot_name="nt_list", slot_type=NamedThing, key_name="id", keyed=True)
 
         super().__post_init__(**kwargs)
 
 
 # Enumerations
-class PersonStatus(EnumDefinitionImpl):
 
-    ALIVE = PermissibleValue(text="ALIVE",
-                                 description="the person is living",
-                                 meaning=PATO["0001421"])
-    DEAD = PermissibleValue(text="DEAD",
-                               description="the person is deceased",
-                               meaning=PATO["0001422"])
-    UNKNOWN = PermissibleValue(text="UNKNOWN",
-                                     description="the vital status is not known")
-
-    _defn = EnumDefinition(
-        name="PersonStatus",
-    )
 
 # Slots
 class slots:
@@ -171,21 +112,5 @@ slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
 slots.description = Slot(uri=SCHEMA.description, name="description", curie=SCHEMA.curie('description'),
                    model_uri=CLEANROOM_SCHEMA.description, domain=None, range=Optional[str])
 
-slots.primary_email = Slot(uri=SCHEMA.email, name="primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=CLEANROOM_SCHEMA.primary_email, domain=None, range=Optional[str])
-
-slots.birth_date = Slot(uri=SCHEMA.birthDate, name="birth_date", curie=SCHEMA.curie('birthDate'),
-                   model_uri=CLEANROOM_SCHEMA.birth_date, domain=None, range=Optional[Union[str, XSDDate]])
-
-slots.age_in_years = Slot(uri=CLEANROOM_SCHEMA.age_in_years, name="age_in_years", curie=CLEANROOM_SCHEMA.curie('age_in_years'),
-                   model_uri=CLEANROOM_SCHEMA.age_in_years, domain=None, range=Optional[int])
-
-slots.vital_status = Slot(uri=CLEANROOM_SCHEMA.vital_status, name="vital_status", curie=CLEANROOM_SCHEMA.curie('vital_status'),
-                   model_uri=CLEANROOM_SCHEMA.vital_status, domain=None, range=Optional[Union[str, "PersonStatus"]])
-
-slots.personCollection__entries = Slot(uri=CLEANROOM_SCHEMA.entries, name="personCollection__entries", curie=CLEANROOM_SCHEMA.curie('entries'),
-                   model_uri=CLEANROOM_SCHEMA.personCollection__entries, domain=None, range=Optional[Union[Dict[Union[str, PersonId], Union[dict, Person]], List[Union[dict, Person]]]])
-
-slots.Person_primary_email = Slot(uri=SCHEMA.email, name="Person_primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=CLEANROOM_SCHEMA.Person_primary_email, domain=Person, range=Optional[str],
-                   pattern=re.compile(r'^\S+@[\S+\.]+\S+'))
+slots.dataLists__nt_list = Slot(uri=CLEANROOM_SCHEMA.nt_list, name="dataLists__nt_list", curie=CLEANROOM_SCHEMA.curie('nt_list'),
+                   model_uri=CLEANROOM_SCHEMA.dataLists__nt_list, domain=None, range=Optional[Union[Dict[Union[str, NamedThingId], Union[dict, NamedThing]], List[Union[dict, NamedThing]]]])
